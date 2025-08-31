afb77414d8c5c61e6f67c6f1a75a8dc9f3a1b82e
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-08-31 15:09:09 +0300

    jsx

--- src/elements/Header.jsx
+++ src/elements/Header.jsx
@@ -2,7 +2,7 @@ import bigAva from './../assets/freenas.svg';
 
 export function Header() {
   return <div className='header'>
-    <div className='row'><img src={bigAva} width='80vmin' alt='logo' />
-      <h2>AKula</h2></div>
+    <div className='row'><img src={bigAva} width='60vmin' alt='logo' />
+      <h3>AKula</h3></div>
   </div>;
 }
