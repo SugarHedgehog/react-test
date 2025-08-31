91af81ca52d483dfec4d880bb9c7b0da3d506d16
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-08-31 11:44:13 +0300

    Refactor CSS class names from flex-container-* to semantic column and flex-row

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       </div>
       <div className='row'>
         <div className='menu'>
-          <div className='flex-container-col'>
+          <div className='column'>
             <a href='#s'>My Sea</a>
             <a href='#s'>Diner News</a>
             <a href='#s'>Temperature</a>
@@ -20,18 +20,16 @@ function App() {
         </div>
         <div className='main'>
           <div className='flex-container-main'>
-    <img className='img-preview' src={akula} alt='ava'></img>
-<div className='flex-container-row'>
-            <img className='img-ava' src={akula} alt="akula" width="100" height="100"></img>
-            <div className='flex-container-col'>
-              <p>Name: Ariel</p>
-              <p>Year: 21</p>
-              <p>City: Seawill</p>
+            <img className='img-preview' src={akula} alt='ava'></img>
+            <div className='flex-row'>
+              <img className='img-ava' src={akula} alt="akula" width="100" height="100"></img>
+              <div className='column'>
+                <p>Name: Ariel</p>
+                <p>Year: 21</p>
+                <p>City: Seawill</p>
+              </div>
             </div>
           </div>
-          </div>
-          
-          
         </div>
       </div>
     </div>
