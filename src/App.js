ec7d3c5f51dec24f911b0c253163d5cb8676f1ed
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-08-31 15:18:23 +0300

    more css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import './App.css';
+import './style/App.css';
+import './style/Column.css';
+import './style/Header.css';
+import './style/Img.css';
+import './style/Main.css';
+import './style/Menu.css';
+import './style/Row.css';
+import './style/Text.css';
+
 import { Menu } from './elements/Menu';
 import { Header } from './elements/Header';
 import { Main } from './elements/Main';
--- src/elements/DataAvaAkula.jsx
+++ src/elements/DataAvaAkula.jsx
@@ -2,7 +2,7 @@ import { DataAkula } from './DataAkula';
 import akula from './../assets/akula.png';
 
 export function DataAvaAkula() {
-  return <div className='flex-row'>
+  return <div className='data-row'>
     <img className='img-ava' src={akula} alt="akula" width="100" height="100"></img>
     {DataAkula()}
   </div>;
